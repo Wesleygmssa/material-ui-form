d8da71f2fb3068ee706fadc3560cdeeb7e49a24d
wesley guerra <guerra_ssa@hotmail.com>
2021-03-07 10:39:16 -0300

    criando botão de toogle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {TextField, Grid} from '@material-ui/core';
 import { 
   Conatiner , 
@@ -7,104 +7,121 @@ import {
   ContainerInt,
   BoxTitle,
   BoxForm,
-  GroupButton
 } from './styles';
-import {FaCaretUp} from 'react-icons/fa';
+import {FaCaretUp, FaCaretDown} from 'react-icons/fa';
+
+
 
 function App() {
+
+  const [togleConteudos, setTogleConteudos] = useState(false);
+
+  const handleTogle = () => {
+      // setTogleConteudos(prev => !prev);
+      setTogleConteudos(!togleConteudos);
+  }
+
+  const ConteudoUm = () => {
+    return <div></div>
+  }
+  const ConteudoDois = () => {
+    return (
+      <ContainerInt>
+  
+      <BoxTitle>
+          <Title>Registro</Title>
+            <div className="TitleLine"></div>  
+      </BoxTitle>
+  
+      <BoxForm>
+  
+          <Grid container>
+            <Grid item lg={3} xs={12}>
+                <label>Status</label>
+                 <TextField
+                   id="outlined-helperText"
+                   variant="outlined"
+               />
+            </Grid>
+           </Grid>
+  
+           <Grid container>
+            <Grid item lg={3} xs={12}>
+                <label>Eixo temático</label>
+                 <TextField
+                   id="outlined-helperText"
+                   variant="outlined"
+               />
+            </Grid>
+            <Grid item lg={3} xs={12}>
+                <label>Início período da ocorrência</label>
+                 <TextField
+                    type="date"
+                   id="outlined-helperText"
+                   variant="outlined"
+               />
+            </Grid>
+            <Grid item lg={3} xs={12}>
+                <label>Fim período da ocorrência </label>
+                 <TextField
+                 type="date"
+                   id="outlined-helperText"
+                   variant="outlined"
+               />
+            </Grid>
+           </Grid>
+  
+            <Grid container>
+                <Grid item lg={3} xs={12}>
+                <label>Denunciante</label>
+                 <TextField
+                   id="outlined-helperText"
+                   variant="outlined"
+               />
+            </Grid>
+  
+            <Grid item lg={3} xs={12}>
+                <label>CPF/CNPJ do denunciante</label>
+                 <TextField
+                   id="outlined-helperText"
+                   variant="outlined"
+               />
+            </Grid>
+           </Grid>
+  
+           <Grid container>
+                <Grid item lg={3} xs={12}>
+                <label>Comunicante</label>
+                 <TextField
+                   id="outlined-helperText"
+                   variant="outlined"
+               />
+            </Grid>
+  
+            <Grid item lg={3} xs={12}>
+                <label>CPF/CNPJ do comunicante</label>
+                 <TextField
+                   id="outlined-helperText"
+                   variant="outlined"
+               />
+            </Grid>
+           </Grid>
+      </BoxForm>
+    
+  </ContainerInt>  
+    )
+  }
+  
   return (
     <Conatiner>
 
-          <Button>
+          <Button onClick={handleTogle}>
               Busca Avançada
-             <FaCaretUp size={20}/>
+              {togleConteudos ? <FaCaretUp size={20}/> : <FaCaretDown size={20}/>}
           </Button>
 
-          <ContainerInt>
-
-              <BoxTitle>
-                  <Title>Registro</Title>
-                    <div className="TitleLine"></div>  
-              </BoxTitle>
-
-              <BoxForm>
-
-                  <Grid container>
-                    <Grid item lg={3} xs={12}>
-                        <label>Status</label>
-                         <TextField
-                           id="outlined-helperText"
-                           variant="outlined"
-                       />
-                    </Grid>
-                   </Grid>
-
-                   <Grid container>
-                    <Grid item lg={3} xs={12}>
-                        <label>Eixo temático</label>
-                         <TextField
-                           id="outlined-helperText"
-                           variant="outlined"
-                       />
-                    </Grid>
-                    <Grid item lg={3} xs={12}>
-                        <label>Início período da ocorrência</label>
-                         <TextField
-                            type="date"
-                           id="outlined-helperText"
-                           variant="outlined"
-                       />
-                    </Grid>
-                    <Grid item lg={3} xs={12}>
-                        <label>Fim período da ocorrência </label>
-                         <TextField
-                         type="date"
-                           id="outlined-helperText"
-                           variant="outlined"
-                       />
-                    </Grid>
-                   </Grid>
-
-                    <Grid container>
-                        <Grid item lg={3} xs={12}>
-                        <label>Denunciante</label>
-                         <TextField
-                           id="outlined-helperText"
-                           variant="outlined"
-                       />
-                    </Grid>
-
-                    <Grid item lg={3} xs={12}>
-                        <label>CPF/CNPJ do denunciante</label>
-                         <TextField
-                           id="outlined-helperText"
-                           variant="outlined"
-                       />
-                    </Grid>
-                   </Grid>
-
-                   <Grid container>
-                        <Grid item lg={3} xs={12}>
-                        <label>Comunicante</label>
-                         <TextField
-                           id="outlined-helperText"
-                           variant="outlined"
-                       />
-                    </Grid>
-
-                    <Grid item lg={3} xs={12}>
-                        <label>CPF/CNPJ do comunicante</label>
-                         <TextField
-                           id="outlined-helperText"
-                           variant="outlined"
-                       />
-                    </Grid>
-                   </Grid>
-              </BoxForm>
-            
-          </ContainerInt>  
- 
-          
+          {togleConteudos ? <ConteudoDois /> : <ConteudoUm />}
+  
     </Conatiner>
   );
 }
